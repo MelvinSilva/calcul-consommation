8beedf958403e6167e73a98755ae509b0eb6ef49
Melvin SILVA <melvin.silva@sncf.fr>
2024-10-25 13:58:36 +0200

    fix input date 2

--- src/App.tsx
+++ src/App.tsx
@@ -112,7 +112,7 @@ function App() {
                   type="date"
                   value={date}
                   onChange={(e) => setDate(e.target.value)}
-                  className="w-48 pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition"
+                  className="w-48 h-10 pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition"
                 />
               </div>
             </div>
